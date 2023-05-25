9b21951b4efd1bdf542cb4acd2c75f392a71025a
Célian Louis-Itty <celian.louis-itty@etu.univ-lyon1.fr>
2023-05-26 01:11:50 +0200

    Macro for test class definition !

--- test.hpp
+++ test.hpp
@@ -1,2 +1,28 @@
 #include "testCore/factory.hpp"
 #include "testCore/testBase.hpp"
+
+
+#define TEST(_TEST_NAME_)                                                   \
+void test_##_TEST_NAME_();                                                  \
+class _TEST_NAME_ : public TestBase, RegisteredInFactory<_TEST_NAME_>       \
+{                                                                           \
+public:                                                                     \
+    _TEST_NAME_()                                                           \
+    {                                                                       \
+        FACTORY_INIT;                                                       \
+    }                                                                       \
+                                                                            \
+    static TestBase* CreateInstance()                                       \
+    {                                                                       \
+        return new _TEST_NAME_();                                           \
+    }                                                                       \
+                                                                            \
+    void Run() override                                                     \
+    {                                                                       \
+        std::cout << "Running " << #_TEST_NAME_ << std::endl;               \
+        test_##_TEST_NAME_();                                               \
+    }                                                                       \
+};                                                                          \
+                                                                            \
+void test_##_TEST_NAME_()                                                   \
+
